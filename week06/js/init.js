71888c1113d81df735897f0f5e12a6da2f277375
danigold1020 <danigold2019@g.ucla.edu>
2023-05-18 15:11:54 -0700

    Lab 5-2

--- week06/js/init.js
+++ week06/js/init.js
@@ -1,5 +1,5 @@
 // declare variables
-let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+let mapOptions = {'center': [34.4135868,-119.8496976],'zoom':8}
 
 // use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -8,32 +8,62 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// create a function to add markers
 function addMarker(data){
-    // console.log(message)
-    L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
-    createButtons(lat,lng,location)
-    return message
+    // console.log(data)
+    // these are the names of our lat/long fields in the google sheets:
+    L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data['What is your zip code?']}</h2> <h3>${data['Are you aware of the sources of oil pollution in or near your area?']}</h3>`)
+    createButtons(data.lat,data.lng,data['What is your zip code?'])
+    return
 }
 
+createHomeButton(34.4135868,-119.8496976, "Home")
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng], 13); //this is the flyTo from Leaflet
+    })
+    const spaceForButtons = document.getElementById('placeForButtons')
+    spaceForButtons.appendChild(newButton);//this adds the button to our page.
+}
+
+function createHomeButton(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng], 8); //this is the flyTo from Leaflet
+    })
+    const spaceForButtons = document.getElementById('placeForButtons')
+    spaceForButtons.appendChild(newButton);//this adds the button to our page.
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsfi0OL-ZDjF2GznxLodQinhPIidkyA9jO8EIH82ZUeGyyOR3nBmyKz2nc7cJzgQkiyoanv1c5GIuX/pub?output=csv"
 
 function loadData(url){
     Papa.parse(url, {
         header: true,
         download: true,
-        complete: function(results) {
-            console.log(results.data)
-            for (let i=0; i < results.data.length; i++){
-                console.log(results.data[i].lat)
-                //addMarker(results[i][0],results[i][1])
-                addMarker(data)
-            }
-        }
+        complete: results => processData(results)
     })
 }
 
-const dataURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTgSpMPrWb9h3ccNLJKxO4I1u6BRQP_PdKnzVcfI8CQ6uDYi9YQ7XFJyIousEIBg_4jBJ7KEAuB5qWx/pub?output=csv"
-loadData(dataURL)
+function processData(results){
+    console.log(results)
+    results.data.forEach(data => {
+        console.log(data)
+        addMarker(data)
+    })
+}
 
-const sampleDataArray = [[37,-122],[32,-118],[39,-119],[36,-120]]
+loadData(dataUrl)
+
+
+
 
