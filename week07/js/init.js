b1ebed7c37faff554f4526e69d3305a3eb4eb04b
danigold1020 <danigold2019@g.ucla.edu>
2023-05-27 16:28:16 -0700

    Lab 6

--- week07/js/init.js
+++ week07/js/init.js
@@ -1,12 +1,12 @@
 // declare variables
-let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+let mapOptions = {'center': [34.0709,-118.444],'zoom':10}
 
-let vaccinated = L.featureGroup();
-let nonVaccinated = L.featureGroup();
+let pistachio = L.featureGroup();
+let noPistachio = L.featureGroup();
 
 let layers = {
-    "Vaccinated Respondent": vaccinated,
-    "Unvaccinated Respondent": nonVaccinated
+    "Pistachio": pistachio,
+    "No Pistachio": noPistachio
 }
 
 let circleOptions = {
@@ -18,7 +18,7 @@ let circleOptions = {
     fillOpacity: 0.8
 }
 
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNq8_prhrSwK3CnY2pPptqMyGvc23Ckc5MCuGMMKljW-dDy6yq6j7XAT4m6GG69CISbD6kfBF0-ypS/pub?output=csv"
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQl0VZwxFiHJt97YQdsfw8WWbip9fBrdPRjVLVAwHdPiv89bM5X2H9V4pF93VdiCMUbEdsSutfD1JGy/pub?output=csv"
 
 // define the leaflet map
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -26,26 +26,29 @@ const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 // add layer control box
 L.control.layers(null,layers).addTo(map)
 
-let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-    maxZoom: 16
-});
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-Esri_WorldGrayCanvas.addTo(map);
+L.marker([34.0699, -118.4438]).addTo(map)
+    .bindPopup('UCLA<br> The university I attend.')
+    .openPopup();
 
 function addMarker(data){
-    if(data['Have you been vaccinated?'] == "Yes"){
-        circleOptions.fillColor = "red"
-        vaccinated.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>Vaccinated</h2>`))
-        createButtons(data.lat,data.lng,data['Where did you get vaccinated?'])
+    if(data['Do they sell pistachio ice cream?'] == "Yes"){
+        circleOptions.fillColor = "green"
+        pistachio.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>Pistachio</h2>`))
+        createButtons(data.lat,data.lng,data['What is the name of your favorite ice cream shop in LA?'])
         }
     else{
-        circleOptions.fillColor = "blue"
-        nonVaccinated.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>Non-Vaccinated</h2>`))
-        createButtons(data.lat,data.lng,data['Where did you get vaccinated?'])
+        circleOptions.fillColor = "red"
+        noPistachio.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>No Pistachio</h2>`))
+        createButtons(data.lat,data.lng,data['What is the name of your favorite ice cream shop in LA?'])
     }
     return data
 }
+
+createHomeButton(34.0709,-118.444, "Home")
 
 function createButtons(lat,lng,title){
     const newButton = document.createElement("button"); // adds a new button
@@ -54,11 +57,25 @@ function createButtons(lat,lng,title){
     newButton.setAttribute("lat",lat); // sets the latitude 
     newButton.setAttribute("lng",lng); // sets the longitude 
     newButton.addEventListener('click', function(){
-        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+        map.flyTo([lat,lng], 14); //this is the flyTo from Leaflet
     })
     const spaceForButtons = document.getElementById('placeForButtons')
     spaceForButtons.appendChild(newButton);//this adds the button to our page.
 }
+
+function createHomeButton(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng], 10); //this is the flyTo from Leaflet
+    })
+    const spaceForButtons = document.getElementById('placeForButtons')
+    spaceForButtons.appendChild(newButton);//this adds the button to our page.
+}
+
 
 function loadData(url){
     Papa.parse(url, {
@@ -74,9 +91,9 @@ function processData(results){
         console.log(data)
         addMarker(data)
     })
-    vaccinated.addTo(map) // add our layers after markers have been made
-    nonVaccinated.addTo(map) // add our layers after markers have been made  
-    let allLayers = L.featureGroup([vaccinated,nonVaccinated]);
+    pistachio.addTo(map) // add our layers after markers have been made
+    noPistachio.addTo(map) // add our layers after markers have been made  
+    let allLayers = L.featureGroup([pistachio,noPistachio]);
     map.fitBounds(allLayers.getBounds());
 }
 
